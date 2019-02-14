8f1aeb6e0715259b78eea2344ef7d7329f32390b
faithyoussef <38753427+faithyoussef@users.noreply.github.com>
2019-02-14 14:02:29 +0200

    i hope
    i hope this one works

---
+++ ty_area.py
@@ -0,0 +1,44 @@
+Python 2.6.6 (r266:84297, Aug 24 2010, 18:13:38) [MSC v.1500 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+
+    ****************************************************************
+    Personal firewall software may warn about the connection IDLE
+    makes to its subprocess using this computer's internal loopback
+    interface.  This connection is not visible on any external
+    interface and no data is sent to or received from the Internet.
+    ****************************************************************
+    
+IDLE 2.6.6      
+>>> run
+
+Traceback (most recent call last):
+  File "<pyshell#0>", line 1, in <module>
+    run
+NameError: name 'run' is not defined
+>>> print(n)
+
+Traceback (most recent call last):
+  File "<pyshell#1>", line 1, in <module>
+    print(n)
+NameError: name 'n' is not defined
+>>> print(name);
+
+Traceback (most recent call last):
+  File "<pyshell#2>", line 1, in <module>
+    print(name);
+NameError: name 'name' is not defined
+>>> radius=90
+>>> volume=4/3*3.14*radius
+>>> print(volume);
+282.6
+>>> radius=9
+>>> #assigning radius to number 9
+>>> volume=4/3*3.14*radius
+>>> #computing volume
+>>> print(volume)
+28.26
+>>> z
+[DEBUG ON]
+>>> 
+[DEBUG OFF]
+>>> 
